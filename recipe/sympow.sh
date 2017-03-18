9e88f70d1d860d7a4e1123127d662492d476adb8
Isuru Fernando <isuruf@gmail.com>
2017-03-18 18:47:26 +0530

    Make sure the conda environment is activated

--- recipe/sympow.sh
+++ recipe/sympow.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 SYMPOW_LOCAL="$HOME"/.local/share/sympow
 
+if [ -z "${CONDA_PREFIX}" ]; then
+    echo "conda environment is not activated"
+    exit 1
+fi
+
 if [ ! -d "${SYMPOW_LOCAL}" ]; then
     mkdir -p "${SYMPOW_LOCAL}"
     cp -r "${CONDA_PREFIX}"/share/sympow/* "${SYMPOW_LOCAL}"
