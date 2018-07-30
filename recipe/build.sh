307d46812b6cd5638a8ba9eacbf7fa47106ab7f6
Julian Rüth <julian.rueth@fsfe.org>
2018-07-30 18:11:50 +0200

    Simplify build.sh
    The readme at https://gitlab.com/rezozer/forks/sympow suggests that this is all that is necessary now

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,21 +1,3 @@
-sed -i -e "s|../sympow|sympow_bin|g" generate.c
-sed -i -e "s|./sympow|sympow_bin|g" disk.c new_data
-# Change the relative path of new_data to absolute path
-sed -i -e "s|char PATH\[128\]=\"new_data\"|char PATH\[512\]=\"$PREFIX/bin/sympow_new_data\"|g" generate.c
-
-./Configure
-make
-
-mkdir -p "$PREFIX"/{bin,share/sympow}
-cp -r *.gp datafiles "$PREFIX"/share/sympow
-cp sympow "$PREFIX"/bin/sympow_bin
-cp new_data "$PREFIX"/bin/sympow_new_data
-install -m755 "$RECIPE_DIR"/sympow.sh "$PREFIX"/bin/sympow
-
-pushd "$PREFIX"/share/sympow/datafiles
-
-for file in *.txt; do
-  "$SRC_DIR"/sympow -txt2bin "$(grep -c AT $file)" <$file ${file/txt/bin}
-done
-
-popd
+VARPREFIX=$PREFIX/share sh Configure
+make all
+make install
